e23be53b57b95585d26209dc95bcb1e80bfcc759
rubens <rubenskm@yahoo.com.br>
2014-07-27 13:16:09 -0300

    Update

--- kdtree/src/KdTree.java
+++ kdtree/src/KdTree.java
@@ -28,37 +28,39 @@ public class KdTree {
         if (p == null) return;
         if (contains(p)) return;
 
-        root = insert(root, p, (byte) 0);
+        root = insert(root, p, true, new RectHV(0, 0, 1, 1));
+
     }
 
-    private Node insert(Node node, Point2D p, byte level) {
-        if (node == null) return new Node(p, new RectHV(0, 0, 1, 1), level, 1);
+    private Node insert(Node node, Point2D p, boolean isVertical, RectHV rect) {
+        if (node == null) return new Node(p, rect, isVertical, 1);
 
-        int comp = compare(node, p, level);
-        if (comp < 0) node.left = insert(node.left, p, (byte) (level + 1));
-        else if (comp > 0) node.right = insert(node.right, p, (byte) (level + 1));
+        if (compare(node, p) < 0) {
+            if (node.isVertical) {
+                rect = new RectHV(node.rect.xmin(), node.rect.ymin(), node.point.x(), node.rect.ymax());
+            } else {
+                rect = new RectHV(node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.point.y());
+            }
+            node.left = insert(node.left, p, !node.isVertical, rect);
+        } else {
+            if (node.isVertical) {
+                rect = new RectHV(node.point.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax());
+            } else {
+                rect = new RectHV(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.rect.ymax());
+            }
+
+            node.right = insert(node.right, p, !node.isVertical, rect);
+        }
 
         node.N = 1 + size(node.left) + size(node.right);
         return node;
     }
 
-    private int compare(Node node, Point2D p, byte level) {
-        int cmp;
-        if (level % 2 == 0) {
-            cmp = Point2D.X_ORDER.compare(node.point, p);
+    private int compare(Node node, Point2D p) {
+        if (node.isVertical) {
+            return Point2D.X_ORDER.compare(p, node.point);
         } else {
-            cmp = Point2D.Y_ORDER.compare(node.point, p);
-        }
-
-        if (cmp != 0)
-            return cmp;
-        else {
-            if (level % 2 == 0) {
-                cmp = Point2D.Y_ORDER.compare(node.point, p);
-            } else {
-                cmp = Point2D.X_ORDER.compare(node.point, p);
-            }
-            return cmp;
+            return Point2D.Y_ORDER.compare(p, node.point);
         }
     }
 
@@ -71,7 +73,7 @@ public class KdTree {
         if (node == null) return false;
         if (node.point.equals(point)) return true;
 
-        int cmp = compare(node, point, node.level);
+        int cmp = compare(node, point);
         if (cmp > 0)
             return search(point, node.right);
         else if (cmp < 0)
@@ -86,9 +88,25 @@ public class KdTree {
     }
 
     private void draw(Node node) {
-        if (node == null)
-            return;
-        node.point.draw();
+        if (node == null) return;
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.01);
+
+        Point2D p = node.point;
+        p.draw();
+
+        StdDraw.setPenRadius();
+        if (node.isVertical) {
+            StdDraw.setPenColor(StdDraw.BLUE);
+
+            p.drawTo(new Point2D(p.x(), node.rect.ymin()));
+            p.drawTo(new Point2D(p.x(), node.rect.ymax()));
+        } else {
+            StdDraw.setPenColor(StdDraw.RED);
+
+            p.drawTo(new Point2D(node.rect.xmin(), p.y()));
+            p.drawTo(new Point2D(node.rect.xmax(), p.y()));
+        }
         draw(node.left);
         draw(node.right);
     }
@@ -99,23 +117,30 @@ public class KdTree {
     }
 
     private Iterable<Point2D> range(Node node, RectHV rect) {
-
+        /*
+        To find all points contained in a given query rectangle, start at the root and recursively search for points
+        in both subtrees using the following pruning rule: if the query rectangle does not intersect the rectangle
+        corresponding to a node, there is no need to explore that node (or its subtrees).
+        A subtree is searched only if it might contain a point contained in the query rectangle.
+         */
         if (node == null) return null;
         Queue<Point2D> points = new Queue<Point2D>();
 
-        if (rect.contains(node.point))
-            points.enqueue(node.point);
+        if (rect.intersects(node.rect)) {
+            if (rect.contains(node.point))
+                points.enqueue(node.point);
 
-        if (node.level % 2 == 0) {
-            if (rect.xmin() <= node.point.x())
-                range(node.left, rect);
-            if (rect.xmax() >= node.point.x())
-                range(node.right, rect);
-        } else {
-            if (rect.ymin() <= node.point.y())
-                range(node.left, rect);
-            if (rect.ymax() >= node.point.y())
-                range(node.right, rect);
+            if (node.isVertical) {
+                if (rect.xmin() <= node.point.x())
+                    range(node.left, rect);
+                if (rect.xmax() >= node.point.x())
+                    range(node.right, rect);
+            } else {
+                if (rect.ymin() <= node.point.y())
+                    range(node.left, rect);
+                if (rect.ymax() >= node.point.y())
+                    range(node.right, rect);
+            }
         }
 
         return points;
@@ -123,7 +148,7 @@ public class KdTree {
 
     public Point2D nearest(Point2D p) {
         // a nearest neighbor in the set to p; null if set is empty
-        nearest(root, p, 1.0);
+        nearest(root, p, root.point.distanceSquaredTo(p));
         return minDistancePoint;
     }
 
@@ -145,15 +170,15 @@ public class KdTree {
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
         private Node left;        // the left/bottom subtree
         private Node right;        // the right/top subtree
-        private byte level;
+        private boolean isVertical;
         private int N;
 
-        private Node(Point2D p, RectHV rect, byte level, int N) {
+        private Node(Point2D p, RectHV rect, boolean isVertical, int N) {
             this.point = p;
             this.rect = rect;
             this.left = null;
             this.right = null;
-            this.level = level;
+            this.isVertical = isVertical;
             this.N = N;
         }
     }
